8ccef7c4a71eb3d4b7d98b859fa3da1972f99e7f
Leigh <lcpulzone@gmail.com>
2021-07-07 20:25:48 -0700

    Create Welcome Controller and deploy expect RSpec commands

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -5,11 +5,14 @@ RSpec.describe "Welcome Page" do
   it "has a welcome message" do
     visit root_path
 
+    expect(page).to have_content("Thanks for visiting Viewing Party")
   end
 
-  # it "has a brief description of the application" do
-  #   #stuff
-  # end
+  it "has a brief description of the application" do
+    visit root_path
+
+    expect(page).to have_content("Ya you view movies n stuff")
+  end
   #
   # it "has a BUTTon to log in" do
   #   #stuff
