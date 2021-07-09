8648fcb5be2f1d7e64503350d91b1797783a4498
Leigh <lcpulzone@gmail.com>
2021-07-08 19:41:36 -0700

    Assert three tests

--- spec/features/dashboard/index_spec.rb
+++ spec/features/dashboard/index_spec.rb
@@ -10,6 +10,18 @@ RSpec.describe 'Dashboard Index page' do
   it 'has a button to discover movies' do
     visit dashboard_index_path
 
-    expect(page).to have_button("Discover Movies")
+    expect(page).to have_button("Discover Movies") ###NEED TO ADD TEST FOR ROUTE
+  end
+
+  it 'has a section to show and add friends' do
+    visit dashboard_index_path
+
+    expect(page).to have_content("Friends")
+  end
+
+  it 'has a section for parties' do
+    visit dashboard_index_path
+
+    expect(page).to have_content("Action Movie Partiiiiessss")
   end
 end
