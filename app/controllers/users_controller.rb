834921f8fc4c3fe3c3aff4938f96de74c05784fb
Leigh <lcpulzone@gmail.com>
2021-07-10 12:35:59 -0700

    Rename to users for dev empathy

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  def index; end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = user_params
+    user[:email] = user[:email.downcase]
+    new_user = User.create(user)
+    if new_user.save
+      session[:user_id] = new_user.id
+      flash[:success] = "Welcome, #{new_user.username}!"
+    end
+    redirect_to dashboard_index_path
+  end
+
+  def edit; end
+
+  def update; end
+
+  def destroy; end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
