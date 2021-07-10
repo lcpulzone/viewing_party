545a7d7858e7304882d60ef624737b3b6862c368
Leigh <lcpulzone@gmail.com>
2021-07-10 12:36:36 -0700

    Rename routes to match controller rename

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root 'sessions#index'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
-  get '/register', to: 'register#new'
-  post '/register', to: 'register#create'
+  get '/register', to: 'users#new'
+  post '/register', to: 'users#create'
 
   resources 'dashboard'
 
