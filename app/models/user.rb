5e42923e45f48fdffde3ef9738dc89d1eb927f8c
Leigh <lcpulzone@gmail.com>
2021-07-10 13:35:25 -0700

    Add validations and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  # validates :
   has_many :friendships
   has_many :friendships, foreign_key: :friend_id, dependent: :destroy
   has_many :friends, through: :friendships
 
   has_many :parties
   has_many :invitations
+
+  validates :email, uniqueness: true, presence: true
+
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,10 +8,12 @@ RSpec.describe User, type: :model do
     it { should have_many(:parties) }
     it { should have_many(:invitations) }
   end
-  #
-  # describe 'validations' do
-  #   it { should validate_presence_of(:) }
-  # end
+
+  describe 'validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:password) }
+  end
   #
   # before :each do
   #
