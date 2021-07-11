32880250ab1b89bea8ea469b0e474803f7ff5e33
Leigh <lcpulzone@gmail.com>
2021-07-10 20:12:07 -0700

    Allow rubocop to autocorrect

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,12 +8,12 @@ class SessionsController < ApplicationController
   def create
     found_user = User.find_by(email: params[:email])
     if found_user && found_user.authenticate(params[:password])
-        session[:user_id] = found_user.id
-        flash[:success] = "Welcome, #{found_user.email}!"
-        redirect_to dashboard_index_path if current_user
+      session[:user_id] = found_user.id
+      flash[:success] = "Welcome, #{found_user.email}!"
+      redirect_to dashboard_index_path if current_user
     else
-        flash[:error] = "Your email or password are incorrect"
-        redirect_to "/login"
+      flash[:error] = 'Your email or password are incorrect'
+      redirect_to '/login'
     end
   end
 
