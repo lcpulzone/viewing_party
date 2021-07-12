96d9b8c006d5956d931febcbf735682a0c2a8dfb
Leigh <lcpulzone@gmail.com>
2021-07-11 20:28:39 -0700

    Add initial spec

--- spec/features/movies/index_spec.rb
+++ spec/features/movies/index_spec.rb
@@ -6,4 +6,25 @@ RSpec.describe 'Movies Discover Page' do
 
     expect(page).to have_button("Top Rated Movies")
   end
+
+  it 'has a form to search for movies' do
+    visit discovermovies_path
+
+    expect(page).to have_content("")
+  end
 end
+
+# As an authenticated user,
+# When I visit the '/discover' path
+# I should see
+#
+#  Button to Discover top 40 movies
+# Details When the user clicks on the
+# top 40 button they should be taken to the movies page.
+#
+#  A text field to enter keyword(s) to search by movie title
+#  A Button to Search by Movie Title
+# Details When the user clicks on the Search button
+# they should be taken to the movies page
+#
+# The movies will be retrieved by consuming The MovieDB API
