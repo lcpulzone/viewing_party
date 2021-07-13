85ff371bfd09396e5a4769a8586dfb47a4caf33b
ignored-comment <info.konham@gmail.com>
2021-07-12 21:57:04 -0400

    Create MovieFacade spec. ln 4-7 assertions passing

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,5 +1,5 @@
 class MovieFacade
   def self.top_40_movies
-    MovieService.get_top_40_movies_part_1.merge(MovieService.get_top_40_movies_part_2)
+    MovieService.get_top_40_movies_part_1[:results].concat(MovieService.get_top_40_movies_part_2[:results])
   end
 end
---
+++ spec/facades/movies_facade_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+
+describe 'MoviesFacade' do
+  it 'can list the top 40 results of the MovieServices.top_40 methods' do
+    expect(MovieFacade.top_40_movies.class).to eq(Array)
+    expect(MovieFacade.top_40_movies.count).to eq(40)
+  end
+end
