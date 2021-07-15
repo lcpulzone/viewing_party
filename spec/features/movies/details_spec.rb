d0fd717322d56db04b4e1116140f3f402224ab52
ignored-comment <info.konham@gmail.com>
2021-07-15 00:59:25 -0400

    Details viewing party

--- spec/features/movies/details_spec.rb
+++ spec/features/movies/details_spec.rb
@@ -17,6 +17,17 @@ RSpec.describe 'Movie Details Page' do
     visit "/moviedetails?id=11977"
 
     expect(page).to have_content("Caddyshack")
-    
+  end
+
+  it 'has a button to create a new viewing party' do
+    visit discovermovies_path
+
+    fill_in :search, with: "caddyshack"
+    click_button 'Search'
+    click_link 'Caddyshack'
+
+    expect(page).to have_button("Create A Viewing Party With This Movie!")
+
+    click_button 'Create A Viewing Party With This Movie!'
   end
 end
