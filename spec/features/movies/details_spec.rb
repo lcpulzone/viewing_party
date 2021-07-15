644380a56defd1ad3bb6d36e842018993678dd74
Leigh <lcpulzone@gmail.com>
2021-07-14 19:05:11 -0700

    Add in movie id to routes

--- spec/features/movies/details_spec.rb
+++ spec/features/movies/details_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Movie Details Page' do
   it 'can show the details of a movie' do
-    visit moviedetails_path
+    visit "/moviedetails?id=11977"
 
     expect(page).to have_content("The Deets")
   end
@@ -13,6 +13,8 @@ RSpec.describe 'Movie Details Page' do
     fill_in :search, with: "caddyshack"
     click_button 'Search'
     click_link 'Caddyshack'
+
+    visit "/moviedetails?id=11977"
 
     # expect(page).to have_content()
   end
